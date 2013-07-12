4f99a113b077b4da5c50a897067386ddf6bb9bb6
Rohan Chavan <rohanchavan18@gmail.com>
2013-07-12 19:38:33 +0530

    added view event info permission to menu

--- install.sql
+++ install.sql
@@ -5,7 +5,7 @@ SELECT @adminCampaignWeight := MAX(weight)+1 FROM civicrm_navigation where paren
 INSERT INTO civicrm_navigation
     ( domain_id, url, label, name, permission, permission_operator, parent_id, is_active, has_separator, weight )
 VALUES     
-    ( @domain_id,'civicrm/showevents', 'Show Events', 'Show Events', 'access CiviCRM', 'AND', @administerID, '1', NULL, @adminCampaignWeight );
+    ( @domain_id,'civicrm/showevents', 'Show Events', 'Show Events', 'view event info', 'AND', @administerID, '1', NULL, @adminCampaignWeight );
 
 SELECT @domain_id := min(id) FROM civicrm_domain;
 SELECT @administerID    := MAX(id) FROM civicrm_navigation where name = 'CiviEvent';
@@ -14,4 +14,4 @@ SELECT @adminCampaignWeight := MAX(weight)+1 FROM civicrm_navigation where paren
 INSERT INTO civicrm_navigation
     ( domain_id, url, label, name, permission, permission_operator, parent_id, is_active, has_separator, weight )
 VALUES     
-    ( @domain_id,'civicrm/eventcalendarsettings', 'Event Calendar Settings', 'Event Calendar Settings', 'access CiviCRM', 'AND', @administerID, '1', NULL, @adminCampaignWeight );
+    ( @domain_id,'civicrm/eventcalendarsettings', 'Event Calendar Settings', 'Event Calendar Settings', 'view event info', 'AND', @administerID, '1', NULL, @adminCampaignWeight );
