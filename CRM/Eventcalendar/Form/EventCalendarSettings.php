254dc4ebb0db0cfd178cadbb38637c7b3a872999
Sunil Pawar <sunil@Sunils-MacBook-Pro.local>
2017-07-15 20:30:45 +0700

    corrected block for color set default event type color value

--- CRM/Eventcalendar/Form/EventCalendarSettings.php
+++ CRM/Eventcalendar/Form/EventCalendarSettings.php
@@ -144,9 +144,11 @@ class CRM_Eventcalendar_Form_EventCalendarSettings extends CRM_Core_Form {
     $domainID = CRM_Core_Config::domainID();
     foreach ($existing['values'][$domainID] as $name => $value) {
       $defaults[$name] = $value;
-      // set event type color
-      foreach(json_decode($value, true) as $eventType) {
-        $defaults['eventtype_'.$eventType['id']] = $eventType['color'];
+      if ($name == 'eventcalendar_event_types') {
+        // set event type color
+        foreach(json_decode($value, true) as $eventType) {
+          $defaults['eventtype_'.$eventType['id']] = $eventType['color'];
+        }
       }
     }
     return $defaults;
