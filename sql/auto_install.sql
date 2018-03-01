dbe8e8ef9fcd45cc467f566310044f90cc077923
Omar abu hussein <opetmar91@gmail.com>
2018-03-01 22:19:53 +0000

    Revert back commit 9d12ac57 and only allow users with 'view event info' to access the calendar

--- sql/auto_install.sql
+++ sql/auto_install.sql
@@ -5,7 +5,7 @@ SELECT @adminCampaignWeight := MAX(weight)+1 FROM civicrm_navigation where paren
 INSERT INTO civicrm_navigation
     ( domain_id, url, label, name, permission, permission_operator, parent_id, is_active, has_separator, weight )
 VALUES     
-    ( @domain_id,'civicrm/showevents', 'Show Events Calendar', 'Show Events Calendar', 'access CiviCRM', 'AND', @administerID, '1', NULL, @adminCampaignWeight );
+    ( @domain_id,'civicrm/showevents', 'Show Events Calendar', 'Show Events Calendar', 'view event info', 'AND', @administerID, '1', NULL, @adminCampaignWeight );
 
 SELECT @domain_id := min(id) FROM civicrm_domain;
 SELECT @administerID    := MAX(id) FROM civicrm_navigation where name = 'CiviEvent';
