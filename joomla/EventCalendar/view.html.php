6815b99552d01909a1c4f74479032e9a06ba56d1
vinay-osseed <vinay@osseed.com>
2024-03-27 17:23:36 +0530

    fix: joomla frontend calendar display component

--- joomla/EventCalendar/view.html.php
+++ joomla/EventCalendar/view.html.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('_JEXEC') or die('No direct access allowed'); 
+defined('_JEXEC') or die('No direct access allowed');
 //pulling in the content of the file below.
 //require_once ( JPATH_ADMINISTRATOR .DS.'components'.DS.'com_civicrm'.DS.'CRM'.DS.'Contact'.DS.'Page'.DS.'View'.DS.'UserDashBoard.php' );
 
