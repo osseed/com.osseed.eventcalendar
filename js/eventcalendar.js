9e4b1bacc69b84786a6e87c8b804a3552272f4f7
vinay-osseed <vinay@osseed.com>
2025-06-24 15:44:06 +0530

    refact: streamline show/hide logic for event color input based on checkbox state

--- js/eventcalendar.js
+++ js/eventcalendar.js
@@ -14,15 +14,15 @@ CRM.$(function($) {
     $('input[name="'+label+'"]').val( color );
   }
 
-  $('input[id^=event_]').each(function(){
-    var event_id = $(this).prop('id').replace('event_', '');
-    var n = "eventcolorid_" + event_id;
-    var m = "event_" + event_id;
-    if(!$("#"+m).is( ':checked')) {
-      $("#"+n).parents('.crm-section').hide();
-    }
-    else {
-      $("#"+n).parents('.crm-section').show();
+  // Show/hide based on checkbox
+  $('input[id^=event_]').each(function () {
+    var event_id = this.id.replace('event_', '');
+    var colorInput = $("#eventcolorid_" + event_id);
+    var checkbox = $("#event_" + event_id);
+    if (!checkbox.is(':checked')) {
+      colorInput.parents('.crm-section').hide();
+    } else {
+      colorInput.parents('.crm-section').show();
     }
   });
 });
